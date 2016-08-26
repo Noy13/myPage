d9ed5d35a94a8d77ca315b91042e16e48636cabb
Noy13 <hakobyannoy@hotmail.com>
2016-08-26 12:42:54 +0400

    Add files via upload

---
+++ mysite/test/test.js
@@ -0,0 +1,53 @@
+var check = function(){
+    var point = 0;
+     var radio = document.getElementById("three").checked;
+     var radio1 = document.getElementById("threes").checked;
+     var radio2 = document.getElementById("threess").checked;
+     var radio3 = document.getElementById("foursss").checked;
+     var radio4 = document.getElementById("fivessss").checked;     
+     var radio5 = document.getElementById("twosssss").checked;
+     var radio6 = document.getElementById("fourssssss").checked;
+     var radio7 = document.getElementById("fivesssssss").checked;
+     var radio8 = document.getElementById("fourssssssss").checked;
+     var radio9 = document.getElementById("foursssssssss").checked;
+    if(radio){
+     point ++;
+    }
+    if(radio1){
+        point++;
+    }
+        if(radio2){
+        point++;
+    }
+        if(radio3){
+        point++;
+    }
+        if(radio4){
+        point++;
+    }
+        if(radio5){
+        point++;
+    }
+        if(radio6){
+        point++;
+    }
+        if(radio7){
+        point++;
+    }
+        if(radio8){
+        point++;
+    }
+            if(radio9){
+        point++;
+    }
+    
+    
+     
+    
+    
+    
+    alert(point);
+    
+    
+    
+}
